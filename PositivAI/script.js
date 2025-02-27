4c596877f6a7a08ad5d324a37acf3aeb3ca55e2a
Just_Val <valentin.jardon@protonmail.com>
2025-02-27 20:41:55 +0100

    Add files via upload

--- PositivAI/script.js
+++ PositivAI/script.js
@@ -1,6 +1,8 @@
 const loginForm = document.getElementById("login");
 const continueDiv = document.getElementById("continue_div");
 const sendButton = document.getElementById("send_button");
+let buttonIsDisabled = false;
+
 
 function removeDisclaimer() {
     const element = document.getElementById("disclaimer");
@@ -22,6 +24,11 @@ function login() {
         continueDiv.style.display = "inline-block";
         console.log("a");
     }
+}
+
+// Fonction qui bannit l'utilisation des caractères <>
+function validateInput(input) {
+    input.value = input.value.replace(/[<>]/g, '');
 }
 
 let randomValue = "placeholder";
@@ -48,11 +55,11 @@ function getRandomElement(array) {
     return rValue;
 }
 
-let AsciiArray = ['end', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'];
-
-function getRandomValue() {
-    getRandomElement(AsciiArray);
-}
+// J'ai du supprimer les caractères "<>" car, avec le texte à l'intérieur, ils étaient interprétés comme du code html...
+// Regardez cette magnifique balise : <y 4a="Py?*:Y'^O6M;%&quot;CNzoM.E+n?~G+T0;0:U3#5u"><y>.
+// J'en ai profité pour rajouter les lettres à, À, é et É.
+// Je précise qu'à la base j'utilisais les 95 caractères ASCII et le mot-clé "end" pour définir la fin du message.
+let AsciiArray = ['end', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'à', 'À', 'é', 'É'];
 
 function removeContinueDiv() {
     continueDiv.remove();
@@ -63,9 +70,11 @@ function printQuestion() {
     console.log(userQuestion);
     document.getElementById("send_button").style.cursor = "not-allowed";
     sendButton.setAttribute("disabled", "");
+    buttonIsDisabled = true;
+    getRanEl();
 }
 
-let isFinished = false;
+let isFinished = true;
 let ai_answer = "";
 let aiAnswerField = document.getElementById("ai_answer_field");
 
@@ -75,6 +84,9 @@ function getRanEl() {
 
     if (randomValue == "end") {
 	    isFinished = true;
+        sendButton.removeAttribute("disabled");
+        document.getElementById("send_button").style.cursor = "auto";
+        buttonIsDisabled = false;
     } else {
         writeAiAnswer();
     }
@@ -88,3 +100,39 @@ function writeAiAnswer() {
     ai_answer = ai_answer + randomValue;
     aiAnswerField.innerHTML = ai_answer;
 }
+
+let paragraphCounter = 0;
+function addElement() {
+    // crée un nouvel élément div
+    let newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    let newContent = document.createElement("p");
+    newContent.id = 'paragraph' + paragraphCounter;
+    // ajoute le paragraphe à la nouvelle div créée
+    newDiv.appendChild(newContent);
+  
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    let currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+}
+
+let counter = 0;
+
+function buttonAnimations() {
+    if (buttonIsDisabled == true) {
+        sendButton.classList.remove("animate");
+    } else if (counter == 0) {
+            sendButton.addEventListener("mouseenter", function() {
+                sendButton.classList.add("animate");
+            });
+            
+            sendButton.addEventListener("mouseleave", function() {
+                sendButton.classList.remove("animate");
+            });
+
+            counter = 1;
+    }
+    requestAnimationFrame(buttonAnimations);
+}
+
+requestAnimationFrame(buttonAnimations);
